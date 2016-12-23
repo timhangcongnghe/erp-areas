e23eb1aef9fe938c295fff60b3b5db2df88d638f
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-12-22 19:00:30 -0800

    todo engines areas

--- app/models/erp/areas/state.rb
+++ app/models/erp/areas/state.rb
@@ -8,6 +8,26 @@ module Erp::Areas
       params = params.to_unsafe_hash
       and_conds = []
       
+      # show archived items condition - default: false
+      show_archived = false
+      
+      #filters
+      if params["filters"].present?
+        params["filters"].each do |ft|
+          or_conds = []
+          ft[1].each do |cond|
+            # in case filter is show archived
+            if cond[1]["name"] == 'show_archived'
+              # show archived items
+              show_archived = true
+            else
+              or_conds << "#{cond[1]["name"]} = '#{cond[1]["value"]}'"
+            end
+          end
+          and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
+        end
+      end
+      
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -18,6 +38,12 @@ module Erp::Areas
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
+      
+      # join with users table for search creator
+      query = query.joins(:country)
+      
+      # showing archived items if show_archived is not true
+      query = query.where(archived: false) if show_archived == false
 
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
       
